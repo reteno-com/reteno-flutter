3ca5cadebcb5fcc8d430182c4ed923c392b705bf
Roman Nakonecnhyi <roman.nakonechnyi@yalantis.net>
2023-06-28 13:29:21 +0300

    Update native sdks to last versions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -64,24 +64,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<void> _showAlert(BuildContext context, String text) {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Push data"),
-          content: SingleChildScrollView(child: Text(text)),
-          actions: <Widget>[
-            TextButton(
-              child: const Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+  Future<void> _showAlert(BuildContext context, String text) async {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: SingleChildScrollView(child: Text(text)),
+    ));
   }
 
   final Reteno _reteno = Reteno();
